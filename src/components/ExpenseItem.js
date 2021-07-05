4fd2bc1b7d14e3c86ff172ebe9d6fe7bfeb2a97f
Vishal Joshi <vishalj026@gmail.com>
2021-07-05 21:21:20 +0530

    Added Expense Item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,17 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 7, 5);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.38;
     return (
-        <h2>Expense Item!</h2>
+        <div className="expense-item">
+            <div>{props.date.toLocaleDateString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
     );
 }
 
